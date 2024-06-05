ed4ba5a3ceb811b980a98cbaebbd2c5ed1d95b9c
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-06-05 12:28:38 +0600

    card style new

--- src/page/home/Our_service.jsx
+++ src/page/home/Our_service.jsx
@@ -1,7 +1,12 @@
+
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 const Our_service = () => {
     const [products, setProducts] = useState([]);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
     useEffect(() => {
         const fetchProducts = async () => {
             try {
@@ -18,35 +23,48 @@ const Our_service = () => {
 
         fetchProducts();
     }, []);
+
     return (
         <div className="bg-[#F8F7F0]">
             <div className="max-w-screen-xl mx-auto py-10 px-5">
                 <p style={{ fontFamily: "'Covered By Your Grace', cursive" }} className='text-[#EEC044] text-center text-xl'>Our Services</p>
                 <h1 className="text-center text-4xl font-semibold font-monrope">
-                    What We <span className='text-green-500'>Offer</span>
+                    What We <span className='text-[#4BAF47]'>Offer</span>
                 </h1>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 justify-items-center  my-10">
-                    {
-                        products.map((product) => (
-                            <div key={product.id} className="relative bg-[#FFFFFF] w-64 overflow-hidden">
-                                <img className='h-40 w-64' src={product.img1} alt="" />
-                                <div className="absolute top-[45%] left-3/4 transform -translate-x-1/2 -translate-y-1/2 bg-[#C5CE38] w-16 h-16 rounded-md flex items-center justify-center">
-                                    <img src={product.icon} alt="" className="w-10" />
-                                </div>
-                                <div className='p-5'>
-                                    <div className="mt-5">
-                                        <h1 className="text-xl font-semibold my-2 font-monrope">{product.offerName}</h1>
-                                        <p className="text-[#878680]">{product.title}</p>
-                                    </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 justify-items-center my-10">
+                    {products.map((product, index) => (
+                        <Link
+                            key={product.id}
+                            className={`relative bg-[#FFFFFF] w-64 overflow-hidden transition-shadow duration-300 ${hoveredIndex === index ? 'shadow-md shadow-[#C5CE38]' : 'shadow-none'}`}
+                            onMouseEnter={() => setHoveredIndex(index)}
+                            onMouseLeave={() => setHoveredIndex(null)}
+                        >
+                            <img className='h-40 w-64' src={product.img1} alt="" />
+                            <div
+                                className={`absolute top-[45%] left-3/4 transform -translate-x-1/2 -translate-y-1/2 bg-[${hoveredIndex === index ? '#EEC044' : '#C5CE38'}] w-16 h-16 #C5CE38 rounded-md flex items-center justify-center transition-background-color duration-300`}
+                            >
+                                <img src={product.icon} alt="" className="w-10" />
+                            </div>
+                            <div className='p-5'>
+                                <div className="mt-5">
+                                    <h1 className="text-xl font-semibold my-2 font-monrope">{product.offerName}</h1>
+                                    <p className="text-[#878680]">{product.title}</p>
                                 </div>
                             </div>
-                        ))
-                    }
+                            {hoveredIndex === index && (
+                                <div
+                                    className='flex justify-end items-center px-5 absolute bottom-0 left-0  right-0  h-5 text-white cursor-pointer transition-transform duration-300 bg-[#EEC044] hover:text-[#FFFFFF] hover:translate-x-1 hover:translate-y-1 hover:scale-105'
+                                >
+                                    <ArrowRightAltIcon />
+                                </div>
+                            )}
+                        </Link>
+                    ))}
                 </div>
             </div>
         </div>
     );
 };
 
-export default Our_service;
+export default Our_service
