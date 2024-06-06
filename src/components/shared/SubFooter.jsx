15c84a93770fc0ffe077280f561c1b43cd1ebc69
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-06-06 14:51:50 +0600

    sub footer add

--- src/components/shared/SubFooter.jsx
+++ src/components/shared/SubFooter.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
 
 const SubFooter = () => {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <div>
-            this is the sub footer
+        <div className='bg-[#1F1E17]'>
+            <div className='max-w-screen-xl mx-auto px-5'>
+                <div className='flex flex-col md:flex-row justify-between items-center py-5'>
+                    <div>
+                        <p className='text-gray-400 font-monrope text-center'>© {currentYear} Agrios. All Rights Reserved.</p>
+                    </div>
+                    <div className=''>
+                        <p className='text-gray-400 font-monrope text-center'>
+                            Terms of Use <span>|</span> Privacy Policy
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
