d5be5af9775b38c19b035b8c2cac27df9d879fec
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2024-08-08 16:32:14 +0600

    added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,12 +18,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-  "rules": {
-    "react/no-unescaped-entities": "off",
-    "@typescript-eslint/quotes":"off",
-    "quotes":[0],
-    "avoidEscape":0,
-    "allowTemplateLiterals":0,
-    "no-useless-escape":0
-  }
 }
